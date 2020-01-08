e8248cd7155f5cbd3926f01f6bf9be590542c57b
siddharth <rsiddharth-clustrex@github.com>
2020-01-08 21:20:12 +0530

    bug fix

--- run_basics.py
+++ run_basics.py
@@ -51,7 +51,7 @@ for i, image_path in enumerate(image_path_list):
     # -- save
     name = image_path.strip().split('/')[-1][:-4]
     # np.savetxt(os.path.join(save_folder, name + '.txt'), kpt) 
-    write_obj(os.path.join(save_folder, name + '.obj'), vertices, prn.triangles, colors) #save 3d face(can open with meshlab)
+    write_obj(os.path.join(save_folder, name + '.obj'), vertices, prn.triangles) #save 3d face(can open with meshlab)
     depth = get_depth_image(vertices, prn.triangles, h, w)
     imsave(os.path.join(save_folder, name + '_depth.jpg'), depth_image)
     # sio.savemat(os.path.join(save_folder, name + '_mesh.mat'), {'vertices': vertices, 'colors': colors, 'triangles': prn.triangles})
